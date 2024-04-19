68cfdfe3cf464dcad416ea17b5f2ff9765017454
Ethan Trosper <ethancoletrosper@gmail.com>
2024-04-19 16:46:58 -0400

    Added in classes for String, Float, and Integer Arguments.

---
+++ src/main/java/oop/project/cli/argument/IntegerArgument.java
@@ -0,0 +1,38 @@
+package oop.project.cli.argument;
+
+import oop.project.cli.validator.Validator;
+import oop.project.cli.exception.*;
+import java.util.List;
+
+
+public class IntegerArgument extends BaseArgument<Integer> {
+
+    private Integer value;
+
+    public IntegerArgument(String identifier, boolean isNamed, String name, List<Validator<Integer>> validators,
+                           boolean hasDefault, Integer defaultVal, String help) throws ArgBuildException {
+        super(identifier, isNamed, name, validators, hasDefault, defaultVal, help);
+    }
+
+    @Override
+    public Integer convert(String str) throws ParseException {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new ParseException(String.format("Could not convert argument %s to Integer: %s",
+                    getIdentifier(), e.getMessage()), e);
+        }
+    }
+
+    @Override
+    protected void validate(Integer val) throws ArgParseException {
+        super.validate(val);
+    }
+
+    // ? I am assuming we just want to check if there is a value.
+    @Override
+    public boolean isSatisfied() {
+        return this.value != null || hasDefault();
+    }
+
+}
