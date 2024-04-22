6b7d240bb0bbffd8140d79b06cd5e5acf1dfa02c
Tadania Tait <taitt@ufl.edu>
2024-04-22 18:51:00 -0400

    Deleted unnecesary code.

--- src/main/java/oop/project/cli/argument/builder/StringArgBuilder.java
+++ src/main/java/oop/project/cli/argument/builder/StringArgBuilder.java
@@ -13,58 +13,5 @@ public class StringArgBuilder extends BaseArgBuilder{
     @Override
     public final Argument<String> build() throws ArgBuildException {
         return new StringArgument(identifier, isNamed, name, validators, hasDefault, defaultVal.toString(), help);
-        /*String defaultVal;
-        return new StringArgument(
-                identifier = this.identifier,
-                isNamed = this.isNamed,
-                name = this.name,
-                validators = this.validators,
-                hasDefault = this.hasDefault,
-                defaultVal = this.defaultVal.toString(),
-                setDefault(this.defaultVal);
-                help = this.help
-        );*/
-            /*@Override
-            public String getIdentifier() {
-                return this.getIdentifier();
-            }
-
-            @Override
-            public boolean isNamed() {
-                return false;
-            }
-
-            @Override
-            public String getName() {
-                return null;
-            }
-
-            @Override
-            public String convert(String str) throws ParseException {
-                return null;
-            }
-
-            @Override
-            public boolean hasDefault() {
-                return false;
-            }
-
-            @Override
-            public String getDefault() {
-                return null;
-            }
-
-            @Override
-            public String getHelp() {
-                return null;
-            }
-
-            @Override
-            public boolean isSatisfied() {
-                return false;
-            }
-        };
-        return null;
-    }*/
     }
 }
