e4f9e3e0c84ece837ad10ae5b10fcb63d2d3a3a2
nicklasRice <nicklas.rice@ufl.edu>
2024-04-22 22:06:31 -0400

    namespace operations

--- src/main/java/oop/project/cli/parser/Namespace.java
+++ src/main/java/oop/project/cli/parser/Namespace.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public final class Namespace {
     private final String name;
     private Map<String, Object> parseResults;
-    private Namespace subnamespace;
+    private Namespace subnamespace = null;
 
     Namespace(String name) {
         this.name = name;
@@ -18,5 +18,31 @@ public final class Namespace {
     Namespace createSubnamespace(String name) {
         this.subnamespace = new Namespace(name);
         return this.subnamespace;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Object get(String str) {
+        return this.parseResults.get(str);
+    }
+
+    public Namespace getSubnamespace() {
+        return this.subnamespace;
+    }
+
+    public Namespace getNamespace(String str) {
+        if (this.name.compareTo(str) == 0) {
+            return this;
+        }
+        var subnamespace = this.subnamespace;
+        while (subnamespace.getName().compareTo(str) != 0) {
+            if (subnamespace.getSubnamespace() == null) {
+                return null;
+            }
+            subnamespace = subnamespace.getSubnamespace();
+        }
+        return subnamespace;
     }
 }
